edb97bbc1058da2c6695b5437e802ce38e52998f
Robert M. Lefkowitz <r0ml@mac.com>
2024-03-08 15:31:30 -0500

    another attempt to fix the error introduced by upgrading to XCode 15.3

--- Package.swift
+++ Package.swift
@@ -19,15 +19,15 @@ let package = Package(
             swiftSettings: [.interoperabilityMode(.Cxx)]
             ),
     .target(name: "OpenCVX",
-            dependencies: ["OpenCVbinary"],
+            dependencies: ["OpenCV"],
 //            dependencies: ["OpenCVlocal"],
             path: "wrapper",
 //            swiftSettings: [.interoperabilityMode(.Cxx)],
             cSettings: [.unsafeFlags(["-std=c++11"])],
-            linkerSettings: [.linkedFramework("OpenCVbinary")]
+            linkerSettings: [.linkedFramework("OpenCV")]
             ),
 
-     .binaryTarget(name: "OpenCVbinary",
+     .binaryTarget(name: "OpenCV",
                   url: "https://github.com/r0ml/OpenCV/releases/download/4.9.0/OpenCV.xcframework.zip",
                   checksum: "043b09fc4e0b192fb1ef5880a6058778277c21066f7ceac83bf9e672185c298d"),
 /*
