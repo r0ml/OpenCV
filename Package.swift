38db14090fa2875c19818779366d6f702373f066
Robert Lefkowitz <r0ml@mac.com>
2025-12-09 14:07:50 -0500

    updating to opencv 4.12.0

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.0
 
 import PackageDescription
 
@@ -11,36 +11,11 @@ let package = Package(
     .library(name: "OpenCV",
              targets: ["OpenCV"]
              ),
-    /*
-    .library(
-      name: "SwiftOpenCV",
-      targets: ["SwiftOpenCV" ]),
-     */
   ],
   targets: [
-    /*
-    .target(name: "SwiftOpenCV",
-            dependencies: ["OpenCV_bridge"],
-            path: "swiftWrapper",
-            swiftSettings: [.interoperabilityMode(.Cxx)]
-            ),
-    .target(name: "OpenCV_bridge",
-            dependencies: ["OpenCV"],
-//            dependencies: ["OpenCVlocal"],
-            path: "wrapper",
-            cSettings: [.unsafeFlags(["-std=c++11"])],
-            linkerSettings: [.linkedFramework("OpenCV")]
-            ),
-     */
      .binaryTarget(name: "OpenCV",
                   url: "https://github.com/r0ml/OpenCV/releases/download/4.10.3/OpenCV.xcframework.zip",
                   checksum: "50dd78ae8c26c15bdc77eed9e6aefc07df68830816a5e35ead755e9355fdfe03"),
-/*
-     .binaryTarget(name: "OpenCV",
-                   path: "build_xcframework/OpenCV.xcframework"
-     ),
- */
-
   ]
 )
 
