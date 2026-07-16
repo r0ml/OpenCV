74879080a3a783e48deedbf97d3e6eddd9478dc4
Robert Lefkowitz <r0ml@mac.com>
2026-07-16 05:35:48 -0400

    update to OpenCV 5.0

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
   name: "OpenCV",
   platforms: [
-    .macOS(.v18), .iOS(.v18), .macCatalyst(.v18)
+    .macOS(.v15), .iOS(.v18), // .macCatalyst(.v18)
   ],
   products: [
     .library(name: "OpenCV",
@@ -14,8 +14,8 @@ let package = Package(
   ],
   targets: [
      .binaryTarget(name: "OpenCV",
-                  url: "https://github.com/r0ml/OpenCV/releases/download/4.12.1/OpenCV.xcframework.zip",
-                  checksum: "7396cdb9cd39c9f281460fd2d568a77286d198285457f922f646f589d841e9dd"),
+                  url: "https://github.com/r0ml/OpenCV/releases/download/5.0.0/OpenCV.xcframework.zip",
+                  checksum: "178fb8f90aa991809ba576f4514636ca8637f309a7a047b0fc2a8c581440b83b"),
   ]
 )
 
